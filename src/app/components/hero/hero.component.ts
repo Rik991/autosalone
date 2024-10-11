1f46a56d22ea69bc04a6ebf71f10d594be995a50
Riccardo Santilli <riccardosantilli@live.it>
2024-10-11 16:45:36 +0200

    third-bug fixes

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -13,7 +13,6 @@ export class HeroComponent {
     'https://lh3.googleusercontent.com/proxy/r-fHbK7kFbsXPNyLonJSBOxsKAQGJGuhqO2C3j-G1P4H5opFFtqfXex8-shi5jE82SJ-P3M7yUXYM3qM-u1DbuR_TYn_C8yd2QgglbTC_h8ZinMkk_vSeUKv4dTz6j-OkGie6XqUgnaIEA4FDqfdnsii3zYn5QsSxesJ98Kt__pTxFx0ejSgAEsHKluwsCD8pAJwMVuj_p1ClW8v47yr',
     'https://cdn.veloce.it/wp-content/uploads/webp/2024/07/gt-r-r34-restomod-built-by-legends-1.jpg.webp',
     'https://dimages2.gazzettaobjects.it/files/image_620_388/uploads/2022/01/31/61f7f2726d22c.jpeg',
-    'https://storage.edidomus.it/ListinoAuto/FOTO_A_16_9_640/00001834.jpg',
     'https://media.gqitalia.it/photos/62026959fee22c5f7703f9e3/master/w_1600%2Cc_limit/IPA_IPA28809206.jpg',
     'https://i.pinimg.com/736x/45/b6/5c/45b65cc6d3af413627369380597fe402.jpg',
     'https://cdn.motor1.com/images/mgl/nOzgk/s1/4x3/pontiac-firebird-trans-am-kitt-supercar.webp',
@@ -21,6 +20,7 @@ export class HeroComponent {
     'https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/facelift_2019/model_gw/aventador/2023/02_09_refresh/aven_gate_s_04_m.jpg',
     'https://www.italpassion.fr/wp-content/uploads/2024/04/ferrari-electrique-1024x683.jpg',
     'https://cdn.motor1.com/images/mgl/YyMPl/s3/tesla-model-s-carro-funebre.jpg',
+    'https://storage.edidomus.it/ListinoAuto/FOTO_A_16_9_640/00001834.jpg',
   ];
 
   imgArrayIndex: number = 0;
